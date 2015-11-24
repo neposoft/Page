e88ec32327dde7a8566354675b7eaa1d57550477
Vijay Sapkota <bvsapkota@hotmail.com>
2015-11-24 18:16:46 +0100

    Update frontendRoutes.php

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -4,6 +4,5 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 if (! App::runningInConsole()) {
-    $router->get('{uri}', ['uses' => 'PublicController@uri', 'as' => 'page']);
-    $router->get('/', ['uses' => 'PublicController@homepage', 'as' => 'homepage']);
+    $router->get('/pages/{uri}', ['uses' => 'PublicController@uri', 'as' => 'page']);
 }
